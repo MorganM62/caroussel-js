8ab744518515fc1f3c22646d2d91284abc1c7479
Supramout <moutonmorgan@orange.fr>
2023-06-07 15:00:40 +0200

    final version

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// Ici vous pouvez écrire du code JavaScript
+$(function () {
+    
+    $('.item').click(function () {
+        
+        $(this).addClass("active").siblings('.item').removeClass('active');
+        
+        $(this).next().show().animate({width: '280px'}).siblings('.info').animate({width: 0}, function () {
+            
+            $(this).hide();
+            
+        });
+        
+    });
+
+    $('.btndj').click(function () {
+        $('.item.a').click();
+    });
+    $('.btnvd').click(function () {
+        $('.item.b').click();
+    });
+    $('.btnie').click(function () {
+        $('.item.c').click();
+    });
+    $('.btnja').click(function () {
+        $('.item.d').click();
+    });
+    $('.btnzh').click(function () {
+        $('.item.e').click();
+    });
+    
+});
+
